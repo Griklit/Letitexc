faa9b6b85e47e24752f7bb1925237f616b4bf408
Griklit <griklit@scalt.scalt>
2023-12-19 00:59:44 +0800

    ...

--- src/check.py
+++ src/check.py
@@ -6,18 +6,24 @@ from sys import exc_info
 def check(func: Callable[[], None]):
     try:
         func()
-        print('failed!')
     except Exception as err:
         *_, traceback = exc_info()
+        # last_tb = extract_tb(traceback)[-1]
+        # if last_tb.name == 'raise_me':
+        #     print(f"success! \n"
+        #           f"Exception:\n"
+        #           f"  {err}\n"
+        #           f"  at line {last_tb.lineno}: {last_tb.line}")
+        #     return
         for tb in reversed(extract_tb(traceback)):
             if tb.name == 'raise_me':
                 print(f"success! \n"
                       f"Exception:\n"
                       f"  {err}\n"
                       f"  at line {tb.lineno}: {tb.line}")
-                break
+                return
             elif tb.name == 'answer':
                 print(f"failed!")
-                break
-        else:
-            print('failed!')
+                return
+
+    print('failed!')
