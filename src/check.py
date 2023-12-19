cf9ec87be41a9ab4fbffe8dcfccefe0f9d4d9d9f
Sequence[7] <sequence7@scalt.sct>
2023-12-19 10:41:23 +0800

    优化结果展示

--- src/check.py
+++ src/check.py
@@ -3,6 +3,29 @@ from traceback import extract_tb
 from sys import exc_info
 
 
+def _success(err: Exception, lineno: int, line: str):
+    print(
+        f"{'─' * 64}",
+        f"\033[1;46m{'| Success |':^64}\033[0m",
+        f"{'─' * 64}",
+        '',
+        f"\033[36m<\033[31m{type(err).__name__}\033[0m {err}\033[36m>\033[0m",
+        f"line {lineno}: \033[4m{line}\033[0m",
+        '',
+        f"{'─' * 64}",
+        end='\n', sep='\n'
+    )
+
+
+def _failed():
+    print(
+        f"{'─' * 64}\n",
+        f"\033[1;41m{'| Failed |':^64}\033[0m\n",
+        f"{'─' * 64}\n",
+        end='\n', sep='\n'
+    )
+
+
 def check(func: Callable[[], None]):
     """虽然叫check但是还顺带包含了run的功能"""
     try:
@@ -11,13 +34,16 @@ def check(func: Callable[[], None]):
         *_, traceback = exc_info()
         for tb in reversed(extract_tb(traceback)):
             if tb.name == 'raise_me':
-                print(f"success! \n"
-                      f"Exception:\n"
-                      f"  {err}\n"
-                      f"  at line {tb.lineno}: {tb.line}")
+                _success(err, tb.lineno, tb.line)
                 return
             elif tb.name == 'answer':
-                print(f"failed!")
+                _failed()
                 return
 
-    print('failed!')
+    _failed()
+
+
+if __name__ == '__main__':
+    _success(ValueError('This is an example error str.'), 1, 'asset 1 == 2')
+    # _failed()
+    raise ValueError('This is an example error str.')
