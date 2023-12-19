810f36740c9448fe69cd2021dbd97f1d31a089f1
gwq <email@email.email>
2023-12-19 15:01:55 +0800

    N-05

---
+++ level/normal/N-05.py
@@ -0,0 +1,18 @@
+from src import check
+
+
+def raise_me(alpha):
+    flag = False
+    try:
+        alpha(alpha)[alpha.alpha(alpha)(alpha[alpha])]()()(alpha.alpha.alpha[alpha])
+        flag = True
+    except:
+        pass
+    if flag:
+        raise Exception("Stray from the subject?")
+
+
+@check
+def answer():
+    # write your answer here.
+    raise_me(...)
