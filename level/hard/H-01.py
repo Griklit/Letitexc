a5e6d25003449c9ebc0edfd830aab7171a734b4c
gwq <email@email.email>
2023-12-19 11:51:49 +0800

    收窄 H-01 可能性

--- level/hard/H-01.py
+++ level/hard/H-01.py
@@ -5,7 +5,7 @@ def raise_me(alpha: dict):
     import time
     if type(alpha) is dict:
         if 'key' in alpha:
-            time.sleep(0.1)
+            time.sleep(0.01)
             return alpha['key']
 
 
